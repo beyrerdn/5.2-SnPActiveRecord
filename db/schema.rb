51d13c063958244a869b6392f0a7c367a426e5b4
Daniel Beyrer <beyrerdn@gmail.com>
2015-07-28 22:21:47 -0400

    Completed Active Record models for posts and authors, added association and validation. Added tests for association and validation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150728182702) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "summary"
+    t.datetime "created_at", null: false
+    t.integer  "author_id"
+    t.datetime "updated_at", null: false
+  end
 
 end
